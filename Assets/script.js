8939b55ec3da9c714e153820178345a0b217390f
Connie Wong <conniehhw@gmail.com>
2022-10-03 13:14:05 -0400

    Update to print to box

--- Assets/script.js
+++ Assets/script.js
@@ -9,7 +9,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = password
 }
 
 // Add event listener to generate button
@@ -18,7 +18,80 @@ generateBtn.addEventListener("click", writePassword);
 ////////////////////////////////////////////////////////////////////////////////////////
 
 // This runs the function - when press button "generate password", pop up questions
+
 function myFunction() { 
+  let charLength = prompt("How many characters would you like your password to contain?");
+  if (charLength >= 8 && charLength <=128) {
+    let charNum = "Do you want numbers for your password?";
+    let charSpec = "Do you want special characters in your passoword?"
+    let charUpp = "Do you want upper cases letters in your password?"
+    if (confirm(charNum) == true, confirm(charSpec) == true, confirm(charUpp) == true) {
+        document.getElementById("password").innerHTML = generateRandomNum() + generateRandomSpec() + generateRandomUpp() + generateRandomLow();
+    } else if (confirm(charNum) == false, confirm(charSpec) == true, confirm(charUpp) == true) {
+      document.getElementById("password").innerHTML = generateRandomSpec() + generateRandomUpp + generateRandomLow();
+} else if (confirm(charNum) == false, confirm(charSpec) == false, confirm(charUpp) == true) {
+      document.getElementById("password").innerHTML = + generateRandomUpp + generateRandomLow();
+} else (confirm(charNum) == false, confirm(charSpec) == false, confirm(charUpp) == false) 
+      document.getElementById("password").innerHTML = generateRandomLow();
+      } else {document.getElementById("password").innerHTML = "nothing"}
+}
+  
+  
+  
+ /* if (confirm(charNum) == false, confirm(charSpec) == true, confirm(charUpp) == true) {
+        document.getElementById("password").innerHTML = generateRandomSpec() + generateRandomUpp + generateRandomLow();
+  } else if (confirm(charNum) == false, confirm(charSpec) == false, confirm(charUpp) == true) {
+        document.getElementById("password").innerHTML = + generateRandomUpp + generateRandomLow();
+  } else (confirm(charNum) == false, confirm(charSpec) == false, confirm(charUpp) == false) 
+        document.getElementById("password").innerHTML = generateRandomLow();
+  } else {document.getElementById("password").innerHTML = "nothing"}
+}
+    
+
+
+  /*if (confirm(charNum) == true, confirm(charSpec) == true, confirm(charUpp) == true) {
+    generatePassword = generateRandomNum() + generateRandomSpec() + generateRandomUpp()
+
+
+  /*let charSpec = confirm("Do you want special characters in your password");
+    if (confirm(charSpec) == true) {
+      generateRandomSpec();
+      document.getElementById("demo").innerHTML = generateRandomSpec();
+    } else {
+    generateRandomLow();
+    document.getElementById("demo").innerHTML = generateRandomLow();
+    }     
+  
+
+
+  /*let charSpec = "Do you want special characters in your password";
+      if (confirm(charSpec) == true) {
+        generateRandomSpec();
+        document.getElementById("demo").innerHTML = generateRandomSpec();
+      } else {
+        generateRandomLow();
+        document.getElementById("demo").innerHTML = generateRandomLow();
+      }
+
+  
+  let charUppLow = "Do you want Upper and Lower case letters in your password"; 
+        if (confirm(charUppLow) == true) {
+            generateRandomUpp();
+            document.getElementById("demo").innerHTML = generateRandomUpp();
+        } else {
+        generateRandomLow();
+        document.getElementById("demo").innerHTML = geneateRandomLow();
+      }
+    
+
+      /*let charLength = prompt("Please choose a number between 8 & 128");
+      let charNum = confirm("Do you want numbers for your password?");
+      let charSpec = confirm("Do you want special characters in your password");
+      let charUppLow = confirm("Do you want Upper and Lower case letters in your password"); 
+    }
+  }*/
+  
+/*function myFunction() { 
   let charLength = prompt("How many characters would you like your password to contain?");
   if (charLength >= 8 && charLength <=128) {
     let charNum = confirm("Do you want numbers for your password?");
@@ -28,16 +101,17 @@ function myFunction() {
       prompt("Please choose a number between 8 & 128");
       let charNum = confirm("Do you want numbers for your password?");
       let charSpec = confirm("Do you want special characters in your password");
-      let charUppLow = confirm("Do you want Upper and Lower case letters in your password"); 
-    }
+      let charUppLow = confirm("Do you want Upper and lower case letters in your password"); 
+    } 
   }
+*/
 
-function generateRandomNum () {
-  return Math.floor(Math.random() * 99 ) + 10;
-}
-  console.log(generateRandomNum());
 
-function generateRandomLow () {
+  function generateRandomNum () {
+    return Math.floor(Math.random() * 99 ) + 10;
+    } console.log(generateRandomNum());
+
+  function generateRandomLow () {
   const charDefault = "abcdefghijklmnopqrstuvwxyz"
   return charDefault[Math.floor(Math.random() * charDefault.length)]
   } 
@@ -55,12 +129,22 @@ function generateRandomUpp () {
   }
   console.log(generateRandomUpp());
 
-  /*let charLength = prompt("How many characters would you like your password to contain?");
-  if (charLength <8 || charLength > 128) {
-    prompt("Please choose a number between 8 & 128");
-    let charNum = confirm("Do you want numbers for your password?");
-    let charSpec = confirm("Do you want special characters in your password");
-    let charUppLow = confirm("Do you want Upper and Lower case letters in your password"); */
+/*let text = charNum;
+  if (confirm(text) == true) {
+      generateRandomNum();
+      document.getElementById("demo").innerHTML = generateRandomNum();
+  } else {generateRandomLow();
+    document.getElementById("demo").innerHTML = generateRandomLow();
+  }*/
+
+  /*
+  document.getElementById("demo").innerHTML = charNum;
+
+    generateRandomNum();
+    document.getElementById("demo").innerHTML = generateRandomNum();
+  
+
+
 
 /*
 if (charNum = true) {
